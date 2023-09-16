06065d1de059b11880e1be00a5af2af485623607
Dmitriy Shirochenko <d.shirochenko@criteo.com>
2023-09-16 19:23:19 +0200

    token auth + admin can view groups via endpoints

---
+++ LittleLemonAPI/permissions.py
@@ -0,0 +1,13 @@
+from rest_framework import permissions
+
+class IsManager(permissions.BasePermission):
+    def has_permission(self, request, view):
+        return bool(request.user and request.user.groups.filter(name='Manager').exists())
+
+class IsDeliveryCrew(permissions.BasePermission):
+    def has_permission(self, request, view):
+        return bool(request.user and request.user.groups.filter(name='Delivery Crew').exists())
+
+class IsCustomer(permissions.BasePermission):
+    def has_permission(self, request, view):
+        return bool(request.user and request.user.groups.filter(name='Customer').exists())
--- LittleLemonAPI/views.py
+++ LittleLemonAPI/views.py
@@ -1,6 +1,8 @@
 from rest_framework import generics
+from rest_framework import permissions
 from .models import MenuItem, Category
-from .serializers import MenuItemSerializer, CategorySerializer
+from .serializers import MenuItemSerializer, CategorySerializer, UserSerializer
+from django.contrib.auth.models import User, Group
 
 
 class CategoriesView(generics.ListCreateAPIView):
@@ -19,3 +21,38 @@ class MenuItemListView(generics.ListCreateAPIView):
 class MenuItemDetailView(generics.RetrieveUpdateDestroyAPIView):
     queryset = MenuItem.objects.all()
     serializer_class = MenuItemSerializer
+
+
+class ManagersListView(generics.ListAPIView):
+    permission_classes = [permissions.IsAdminUser]
+    serializer_class = UserSerializer
+
+    def get_queryset(self):
+        managers_group = Group.objects.get(name='Managers')
+        return User.objects.filter(groups=managers_group)
+
+class ManagersRemoveView(generics.DestroyAPIView):
+    permission_classes = [permissions.IsAdminUser]
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+
+    def perform_destroy(self, instance):
+        managers_group = Group.objects.get(name='Managers')
+        instance.groups.remove(managers_group)
+        
+class DeliveryCrewListView(generics.ListAPIView):
+    permission_classes = [permissions.IsAdminUser]
+    serializer_class = UserSerializer
+
+    def get_queryset(self):
+        delivery_group = Group.objects.get(name='Delivery_Crew')
+        return User.objects.filter(groups=delivery_group)
+
+class DeliveryCrewRemoveView(generics.DestroyAPIView):
+    permission_classes = [permissions.IsAdminUser]
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+
+    def perform_destroy(self, instance):
+        delivery_group = Group.objects.get(name='Delivery_Crew')
+        instance.groups.remove(delivery_group)  
