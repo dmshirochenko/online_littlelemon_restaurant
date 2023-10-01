3d09d785a05367014d12cb356645d09b8330b5e8
Dmitriy Shirochenko <d.shirochenko@criteo.com>
2023-10-01 23:04:04 +0200

    postgres setup for render deploy

--- littlelemon/components/database.py
+++ littlelemon/components/database.py
@@ -15,16 +15,24 @@ if DB_CHOICE == "mysql":
         }
     }
 elif DB_CHOICE == "postgres":
-    DATABASES = {
-        "default": {
-            "ENGINE": "django.db.backends.postgresql",
-            "NAME": os.environ.get("POSTGRES_DB"),
-            "USER": os.environ.get("POSTGRES_USER"),
-            "PASSWORD": os.environ.get("POSTGRES_PASSWORD"),
-            "HOST": os.environ.get("POSTGRES_HOST", "127.0.0.1"),
-            "PORT": os.environ.get("POSTGRES_PORT", 5432),
+    if os.environ.get("RENDER"):
+        DATABASES = {
+            "default": dj_database_url.config(
+                default=os.environ.get("DATABASE_URL"),
+                conn_max_age=600,
+            )
         }
-    }
+    else:
+        DATABASES = {
+            "default": {
+                "ENGINE": "django.db.backends.postgresql",
+                "NAME": os.environ.get("POSTGRES_DB"),
+                "USER": os.environ.get("POSTGRES_USER"),
+                "PASSWORD": os.environ.get("POSTGRES_PASSWORD"),
+                "HOST": os.environ.get("POSTGRES_HOST", "127.0.0.1"),
+                "PORT": os.environ.get("POSTGRES_PORT", 5432),
+            }
+        }
 elif DB_CHOICE == "sqlite":
     DATABASES = {
         "default": {
