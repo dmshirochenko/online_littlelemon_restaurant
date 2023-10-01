13676d2171d73c0240a2ca1bd4b7f99dc56f9468
Dmitriy Shirochenko <d.shirochenko@criteo.com>
2023-10-01 18:22:11 +0200

    postgres linter issue fix

--- littlelemon/components/database.py
+++ littlelemon/components/database.py
@@ -16,15 +16,15 @@ if DB_CHOICE == "mysql":
     }
 elif DB_CHOICE == "postgres":
     DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.postgresql",
-        "NAME": os.environ.get("POSTGRES_DB"),
-        "USER": os.environ.get("POSTGRES_USER"),
-        "PASSWORD": os.environ.get("POSTGRES_PASSWORD"),
-        "HOST": os.environ.get("POSTGRES_HOST", "127.0.0.1"),
-        "PORT": os.environ.get("POSTGRES_PORT", 5432),
+        "default": {
+            "ENGINE": "django.db.backends.postgresql",
+            "NAME": os.environ.get("POSTGRES_DB"),
+            "USER": os.environ.get("POSTGRES_USER"),
+            "PASSWORD": os.environ.get("POSTGRES_PASSWORD"),
+            "HOST": os.environ.get("POSTGRES_HOST", "127.0.0.1"),
+            "PORT": os.environ.get("POSTGRES_PORT", 5432),
+        }
     }
-}
 elif DB_CHOICE == "sqlite":
     DATABASES = {
         "default": {
