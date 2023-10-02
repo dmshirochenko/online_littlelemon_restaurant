02c95cef15c237a0d84e3ced2fcf2d2c95419db1
Dmitriy Shirochenko <d.shirochenko@criteo.com>
2023-10-02 12:34:38 +0200

    check if superuser exists has been added

--- build.sh
+++ build.sh
@@ -7,5 +7,10 @@ poetry install
 python manage.py collectstatic --no-input
 python manage.py migrate
 
-# Create Superuser
-echo "from django.contrib.auth import get_user_model; User = get_user_model(); User.objects.create_superuser('$DJANGO_SUPERUSER_USERNAME', '$DJANGO_SUPERUSER_EMAIL', '$DJANGO_SUPERUSER_PASSWORD')" | python manage.py shell
+# Check and Create Superuser if not exists
+echo "
+from django.contrib.auth import get_user_model;
+User = get_user_model();
+if not User.objects.filter(username='$DJANGO_SUPERUSER_USERNAME').exists():
+    User.objects.create_superuser('$DJANGO_SUPERUSER_USERNAME', '$DJANGO_SUPERUSER_EMAIL', '$DJANGO_SUPERUSER_PASSWORD')
+" | python manage.py shell
